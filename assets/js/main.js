caf3c0963d1b66d904ad78421352386f17a1b09f
Pia Varas <p.varas@live.cl>
2021-11-06 17:32:14 -0300

    se agraga el proyecto

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+    $("[data-bs-toggle='tooltip']").tooltip();
+});
+$(function(){
+    $("#correo-enviado").click (function(){
+        alert("Resibiras un correo muy pronto. Gracias por preferirnos!");
+    });
+});
+$(function(){
+    $('[data-bs-toggle="popover"]').popover();
+})
